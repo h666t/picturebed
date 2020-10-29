6382e1aad2211fbf4f0de2ce3e8ac92e3078a60f
h666t <1845720928@qq.com>
2020-10-29 22:45:12 +0800

    引入form表单，从antd

--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -1,6 +1,14 @@
 import React, {useState} from 'react';
 import {observer} from 'mobx-react';
 import useStore from '../stores';
+import { Form, Input, Button, Checkbox } from 'antd';
+const layout = {
+  labelCol: { span: 8 },
+  wrapperCol: { span: 16 },
+};
+const tailLayout = {
+  wrapperCol: { offset: 8, span: 16 },
+};
 
 const Register = observer<React.FC>(() => {
   const {AuthStore} = useStore()
@@ -9,11 +17,49 @@ const Register = observer<React.FC>(() => {
     AuthStore.setUsername(name)
     setUsername(name)
   }
+  const onFinish:(values: any)=>void = values => {
+    console.log('Success:', values);
+  };
+
+  const onFinishFailed:(errorInfo: any)=>void = errorInfo => {
+    console.log('Failed:', errorInfo);
+  };
   return (
     <>
-      Register : {username}
-      <input type="text" onChange={(e)=>{changeUsername(e.target.value)}}/>
-    </>
+      <Form
+        {...layout}
+        name="basic"
+        initialValues={{ remember: true }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+      >
+        <Form.Item
+          label="Username"
+          name="username"
+          rules={[{ required: true, message: 'Please input your username!' }]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          label="Password"
+          name="password"
+          rules={[{ required: true, message: 'Please input your password!' }]}
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <Form.Item {...tailLayout} name="remember" valuePropName="checked">
+          <Checkbox>Remember me</Checkbox>
+        </Form.Item>
+
+        <Form.Item {...tailLayout}>
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+      </>
   )
 })
 
