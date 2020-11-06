4e3041e2d5247c1ce98b01e3d0cb2051448279e0
h666t <1845720928@qq.com>
2020-11-06 21:36:15 +0800

    将History 模块化

---
+++ src/components/history/HistoryResult.tsx
@@ -0,0 +1,75 @@
+import {List, Pagination} from 'antd';
+import dayjs from 'dayjs';
+import React, {useEffect, useState} from 'react';
+import styled from 'styled-components';
+import {observer} from 'mobx-react';
+import useStore from '../../stores/indexStore';
+const Img = styled.img`
+  object-fit: contain;
+`
+const Wrapper = styled.div`
+  max-width: 500px;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  margin: auto;
+  >.pages{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-top: 20px;
+  }
+`
+const HistoryResult = observer (
+  () => {
+    const {Store} = useStore();
+    const [current,setCurrent] = useState(1)
+    const [length,setLength] = useState(0) //用于决定分页的大小
+    useEffect(()=>{
+      Store.imageStore.fetchHistoryStateLength().then((r:any)=>{setLength(r)})
+    },[])
+    useEffect(
+      ()=>{Store.imageStore.fetchHistoryState(current - 1, 5,
+        (x)=>{ setHistoryState(x)} )}, [current,Store.imageStore]
+    )
+    const [historyState,setHistoryState] = useState<{id:string,name:string,url:string,createdAt:any}[]>([])
+    const onChange = (page: number) => {
+      setCurrent(page)
+    };
+    return (
+      <Wrapper>
+        <List
+          itemLayout="horizontal"
+          dataSource={historyState}
+          split={true}
+          loading={Store.imageStore.isHistoryDataLoading}
+          renderItem={item => (
+            <List.Item key={item.id}
+                       extra={
+                         <Img
+                           width={50}
+                           height={50}
+                           alt="logo"
+                           src={item.url}
+                         />
+                       } >
+              <List.Item.Meta
+                title={<a href="https://ant.design">{'文件名：' + item.name}</a>}
+                description={
+                  <div>
+                    <div>创建时间：{JSON.stringify(dayjs(item.createdAt).format('YYYY年MM月DD日hh:mm'))}</div>
+                    <div>url：{item.url}</div>
+                  </div>
+                }
+              />
+            </List.Item>
+          )}
+        />
+        <Pagination className={'pages'} onChange={onChange} defaultCurrent={1} defaultPageSize={5} total={length}/>
+      </Wrapper>
+    )
+  }
+)
+
+export default HistoryResult
--- src/view/History.tsx
+++ src/view/History.tsx
@@ -1,79 +1,18 @@
-import React, {useEffect, useState} from 'react';
-import {Pagination, Empty,List,Avatar } from 'antd';
+import React from 'react';
+import { Empty} from 'antd';
 import useStore from '../stores/indexStore';
-import {User} from 'leancloud-storage';
-import styled from 'styled-components';
-import dayjs from 'dayjs';
+
 import {observer} from 'mobx-react';
-
-const Wrapper = styled.div`
-  max-width: 500px;
-  left: 0;
-  right: 0;
-  top: 0;
-  bottom: 0;
-  margin: auto;
-  >.pages{
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    margin-top: 20px;
-  }
-`
-const Img = styled.img`
-  object-fit: contain;
-`
-
+import HistoryResult from '../components/history/HistoryResult';
 const History = observer( () => {
-  const [current,setCurrent] = useState(1)
-  const onChange = (page: number) => {
-    setCurrent(page)
-  };
+
   const {Store} = useStore();
-  const [historyState,setHistoryState] = useState<{id:string,name:string,url:string,createdAt:any}[]>([])
-  const [length,setLength] = useState(0) //用于决定分页的大小
-  useEffect(()=>{
-    Store.imageStore.fetchHistoryStateLength().then((r:any)=>{setLength(r)})
-  },[])
-  useEffect(
-    ()=>{Store.imageStore.fetchHistoryState(current - 1, 5,
-      (x)=>{ setHistoryState(x)} )}, [current,Store.imageStore]
-  )
 
   return (
     <>
       {
-        Store.AuthStore.values.username && historyState.length > 0
-          ? <Wrapper>
-            <List
-              itemLayout="horizontal"
-              dataSource={historyState}
-              split={true}
-              loading={Store.imageStore.isHistoryDataLoading}
-              renderItem={item => (
-                <List.Item key={item.id}
-                           extra={
-                             <Img
-                               width={50}
-                               height={50}
-                               alt="logo"
-                               src={item.url}
-                             />
-                           } >
-                  <List.Item.Meta
-                    title={<a href="https://ant.design">{'文件名：' + item.name}</a>}
-                    description={
-                      <div>
-                        <div>创建时间：{JSON.stringify(dayjs(item.createdAt).format('YYYY年MM月DD日hh:mm'))}</div>
-                        <div>url：{item.url}</div>
-                      </div>
-                    }
-                  />
-                </List.Item>
-              )}
-            />
-            <Pagination className={'pages'} onChange={onChange} defaultCurrent={1} defaultPageSize={5} total={length}/>
-          </Wrapper>
+        Store.AuthStore.values.username
+          ? <HistoryResult/>
           :<Empty image={Empty.PRESENTED_IMAGE_SIMPLE} />
       }
     </>
